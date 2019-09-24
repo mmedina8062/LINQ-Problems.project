c29b659f3160a350dbb751e0cdac02682142504b
unknown <mmedina8062@gmail.com>
2019-09-24 13:51:06 -0500

    added function for problem 4, tested = works

--- LINQ-Problems/LINQ-Problems/Program.cs
+++ LINQ-Problems/LINQ-Problems/Program.cs
@@ -20,13 +20,42 @@ namespace LINQ_Problems
 
 
             //Problem two
-            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            /*List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
             List<string> distinct = names.Distinct().ToList();
             foreach(var name in distinct)
             {
                 Console.WriteLine(name);
+            }*/
+
+            //Problem three
+            /*     List<string> classGrades = new List<string>(){"80, 100, 92, 89, 65","93, 81, 78, 84, 69","73, 88, 83, 99, 64","98, 100, 66, 74, 55",};
+                 var studentGrade = new List<int>();
+                 double classGradeAverage = 0;
+
+                 foreach (var grade in classGrades)
+                 {
+                     studentGrade = grade.Split(',').Select(g => int.Parse(g)).ToList();
+                     var gradeValue = studentGrade.Where(s => s != studentGrade.Min()).Average();
+                     classGradeAverage += gradeValue;
+                 }
+                 Console.WriteLine(classGradeAverage / classGrades.Count);*/
+
+
+
+            //List<string> splitList = classGrades.Split(",").Where()
+            //List<int> allNumbers = classGrades.SelectMany(x => x.Split(' ')).Select(int.Parse).ToList();
+
+
+            //Problem Four
+            var frequency = from f in "terrill".ToList()
+                            group f by f into letterfrequency
+                            orderby letterfrequency.Key
+                            select letterfrequency;
+
+            foreach (var f in frequency)
+            {
+                Console.WriteLine($"{f.Key}{f.Count()}");
             }
-            
 
 
 
