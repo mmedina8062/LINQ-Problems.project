ea323f77c5aeb3fb6b912881dc701b1e2f7e91ce
unknown <mmedina8062@gmail.com>
2019-09-24 10:05:08 -0500

    created function for List<string> word to capture all words that contains th and tested method = works

---
+++ LINQ-Problems/LINQ-Problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var selectedWords = words.Where(w => w.Contains("th"));
+            foreach (var word in selectedWords)
+            {
+                Console.WriteLine(word);
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
